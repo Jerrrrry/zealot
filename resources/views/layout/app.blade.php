061e74214788b5e98c989df916d49d40b2c6808e
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-19 00:33:02 -0700

    yubin:update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,19 +1,47 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="description" content="">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+        <!-- Title -->
         <title>Cannabis Zealot</title>
-        <link rel="shortcut icon" href="/images/fav_icon.png" type="image/x-icon">
-        <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css'>
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/cards.css">
+
+        <!-- Favicon -->
+        <link rel="icon" href="/img/core-img/favicon.ico">
+
+        <!-- Core Stylesheet -->
+        <link href="/css/style.css" rel="stylesheet">
+
+        <!-- Responsive CSS -->
+        <link href="/css/responsive/responsive.css" rel="stylesheet">
 
     </head>
     <body>
-        @component('layout.nav')@endcomponent
-        <div class="container">
-            
-            @yield('content')
+        @component('components.loading')@endcomponent
+        <!-- Background Pattern Swither -->
+        <div id="pattern-switcher">
+            Bg Pattern
         </div>
-        <script src="/js/bulma.js"></script>
+        <div id="patter-close">
+            <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        @component('components.topheader')@endcomponent
+        @component('components.header')@endcomponent
+            
+        @yield('content')
+        <!-- Jquery-2.2.4 js -->
+        <script src="/js/jquery/jquery-2.2.4.min.js"></script>
+        <!-- Popper js -->
+        <script src="/js/bootstrap/popper.min.js"></script>
+        <!-- Bootstrap-4 js -->
+        <script src="/js/bootstrap/bootstrap.min.js"></script>
+        <!-- All Plugins JS -->
+        <script src="/js/others/plugins.js"></script>
+        <!-- Active JS -->
+        <script src="/js/active.js"></script>
+        
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,5 @@
 @extends('layout.app')
 
 @section('content')
-        <div id="flow">
-            <span class="flow-1"></span>
-            <span class="flow-2"></span>
-            <span class="flow-3"></span>
-        </div>
+        
 @endsection
