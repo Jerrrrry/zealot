d95a700f7fada2cf31dd917261914abd2cf85d16
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-21 01:10:52 -0700

    yubin:update

--- resources/views/marijuana.blade.php
+++ resources/views/marijuana.blade.php
@@ -1,5 +1,16 @@
 @extends('layout.app')
 
 @section('content')
-  
+ <!-- ****** Breadcumb Area Start ****** -->
+ <div class="breadcumb-area" style="background-image: url(img/bg-img/breadcumb.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="bradcumb-title text-center">
+                        <h2>{{$marijuana['data']['Name']}}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
 @endsection
