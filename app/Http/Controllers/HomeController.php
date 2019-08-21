eb964b6161d5b5df2b2a1cf789a7fbfbf5ef2c83
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-20 20:58:09 -0700

    yubin:update

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        $files=scandir('/var/www/html/zealot/public/marijuana/lists/');
+        $results=[];
+        foreach($files as $file)
+        {
+            if($file!=='sample.json'&&$file!=='.'&&$file!=='..')
+            {
+                $results[]=json_decode(file_get_contents("/var/www/html/zealot/public/marijuana/lists/$file"),true);
+            }
+        }
+        
+        return view('welcome',['ms' => $results]);
+    }
+}
