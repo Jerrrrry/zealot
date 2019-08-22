aa498cb8abb2415b2c3c170e219b5a412ae67c59
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-21 17:22:15 -0700

    yubin:update the top img

--- resources/views/marijuana.blade.php
+++ resources/views/marijuana.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
  <!-- ****** Breadcumb Area Start ****** -->
- <div class="breadcumb-area" style="background-image: url(/img/bg-img/breadcumb.jpg);">
+ <div class="breadcumb-area" style="background-image: url(/img/bg-img/mj.jpg);">
         <div class="container h-100">
             <div class="row h-100 align-items-center">
                 <div class="col-12">
