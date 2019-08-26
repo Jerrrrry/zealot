d5c6024dea3c6c04d4d9de26a1ae3f687f5fad70
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-26 14:08:38 -0700

    fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -11,9 +11,8 @@
                             <div class="collapse navbar-collapse justify-content-center" id="yummyfood-footer-nav">
                                 <ul class="navbar-nav">
                                     <li class="nav-item active">
-                                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                                     </li>
-
                                 </ul>
                             </div>
                         </nav>
