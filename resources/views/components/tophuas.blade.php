62c3d031330e06cdf85d7a967cd796da293af922
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-26 00:22:01 -0700

    add

--- resources/views/components/tophuas.blade.php
+++ resources/views/components/tophuas.blade.php
@@ -1,12 +1,16 @@
 @foreach (Cache::get('top-huas') as $hua)
 <!-- Single Popular Post -->
 <div class="single-populer-post d-flex">
-    <img src="/marijuana/images/{{$hua['name']}}.jpg" alt="">
+    <a href="/marijuana-data/{{$hua['name']}}">
+        <img src="/marijuana/images/{{$hua['name']}}.jpg" alt="">
+    </a>
     <div class="post-content">
-        <a href="#">
+        <a href="/marijuana-data/{{$hua['name']}}">
             <h6>{{$hua['data']['Name']}}</h6>
         </a>
         <p>{{$hua['data']['Type']}}</p>
+        <p>{{$hua['data']['Info']}}</p>
+        <p>{{$hua['data']['Origins']}}</p>
     </div>
 </div>
 @endforeach
