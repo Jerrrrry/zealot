23c50695479bac3c493462106be55ce54b4c7436
Yubin QIU <Yubin.Qiu@kp.org>
2019-08-29 17:18:21 -0700

    update

--- resources/views/marijuana.blade.php
+++ resources/views/marijuana.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.app')
-@section('title', 'Marijuana Stain '.$marijuana['data']['Name']|' What is '.$marijuana['data']['Name'].'|Where to buy Weed Seeds')
+@section('title', 'Marijuana Stain '.$marijuana['data']['Name']'|What is '.$marijuana['data']['Name'].'|Where to buy Weed Seeds')
 @section('keywords', $marijuana['data']['Name'].','.$marijuana['data']['Type'].','.$marijuana['data']['Info'].','.$marijuana['data']['Origins'])
 @section('description',$marijuana['data']['Name'].' is '.$marijuana['data']['Type'].' orgins '.$marijuana['data']['Origins'].' flowering is about '.$marijuana['data']['Flowering'].'. '.$marijuana['data']['Name'].' harvest in '.$marijuana['data']['Harvest'])
 @section('dctitle','Marijuana Stain '.$marijuana['data']['Name'].' What is '.$marijuana['data']['Name'])
