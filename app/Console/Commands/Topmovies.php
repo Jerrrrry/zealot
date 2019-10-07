f728a76f2853fb4309bfda068989677a79f1808c
Yubin QIU <Yubin.Qiu@kp.org>
2019-10-06 21:40:40 -0700

    update

---
+++ app/Console/Commands/Topmovies.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Topmovies extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'topmovies';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch 24 top movies from live';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $per_page=24;
+        $query="per_page=$per_page&&categories=44";
+        
+        try{
+            $client=new \GuzzleHttp\Client();
+            $response=$client->get("https://loveplanet.live/wp-json/wp/v2/posts?$query");
+            //return $response->getHeaders()['X-WP-TotalPages'];
+            $data=[];
+            $results=json_decode($response->getBody(),true);
+            //return $totalpages;
+            foreach($results as $result)
+            {
+                $time=Carbon::parse($result['date']);
+                $this->info($result['slug']);
+                $imageid=$result['featured_media'];
+                $data[]=array(
+                'data'=>$result,
+                'image'=>Helper::featureMediumImage($imageid),
+                'date'=>$time->day,
+                'month'=>$time->format('F'),
+                );
+            }
+            Cache::forever('topmovies',$data);
+            
+        }catch(\GuzzleHttp\Exception\ClientException $ce){
+            $this->error($ce->getMessge());
+        }catch(\GuzzleHttp\Exception\RequestException $re){
+            $this->error($rs->getMessge());
+        }catch(\Exception $e){
+            $this->error($e->getMessge());
+        }
+    }
+}
